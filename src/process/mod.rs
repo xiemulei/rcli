f0894dc40a98f419b10a22538011be78c5ceb926
xiemulei <xiemulei@gmail.com>
2024-11-24 21:19:54 +0800

    feature: support static file http serve

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -2,8 +2,10 @@ mod b64;
 mod csv_convert;
 mod gen_pass;
 mod text;
+mod http_serve;
 
 pub use b64::{process_decode, process_encode};
 pub use csv_convert::process_csv;
 pub use gen_pass::process_genpass;
 pub use text::{process_text_sign, process_text_verify, process_text_generate};
+pub use http_serve::process_http_serve;
