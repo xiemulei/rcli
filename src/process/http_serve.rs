a29cb12dd0b829c2acc06f4aa51e050b64da2593
xiemulei <xiemulei@gmail.com>
2024-11-24 22:03:03 +0800

    feature: add tower_http support

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,11 +1,12 @@
 use anyhow::Result;
-use axum::extract::{Path, State};
-use axum::http::StatusCode;
-use axum::routing::get;
-use axum::Router;
-use std::net::SocketAddr;
-use std::path::PathBuf;
-use std::sync::Arc;
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    routing::get,
+    Router,
+};
+use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
 
 #[derive(Debug)]
@@ -15,14 +16,16 @@ struct HttpServeState {
 
 pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    info!("Serve {:?} on {}", path, addr);
+    info!("Serving {:?} on {}", path, addr);
 
-    let state = HttpServeState { path };
+    let state = HttpServeState { path: path.clone() };
+    // axum router
     let router = Router::new()
+        .nest_service("/tower", ServeDir::new(path))
         .route("/*path", get(file_handler))
         .with_state(Arc::new(state));
 
-    let listener = tokio::net::TcpListener::bind(&addr).await?;
+    let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, router).await?;
     Ok(())
 }
@@ -33,22 +36,40 @@ async fn file_handler(
 ) -> (StatusCode, String) {
     let p = std::path::Path::new(&state.path).join(path);
     info!("Reading file {:?}", p);
-
     if !p.exists() {
         (
             StatusCode::NOT_FOUND,
-            format!("File {:?} not found", p.display()),
+            format!("File {} note found", p.display()),
         )
     } else {
+        // TODO: test p is a directory
+        // if it is a directory, list all files/subdirectories
+        // as <li><a href="/path/to/file">file name</a></li>
+        // <html><body><ul>...</ul></body></html>
         match tokio::fs::read_to_string(p).await {
             Ok(content) => {
                 info!("Read {} bytes", content.len());
                 (StatusCode::OK, content)
             }
             Err(e) => {
-                warn!("Error reading file {:?}", e);
+                warn!("Error reading file: {:?}", e);
                 (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.trim().starts_with("[package]"));
     }
 }
